ae6565819de4b0316d2b9ebf54defb6dfb9b8a5e
drewwmercer <awmercer@gmail.com>
2020-12-28 11:17:52 -0500

    Added submission form

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ class Todo(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
     return render_template('index.html')
 
